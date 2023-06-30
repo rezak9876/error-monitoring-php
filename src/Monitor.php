f5d55ac4c6bea2bf4b8ec70c2e5a65cc24459e2b
rezak9876 <rezak9876.official@gmail.com>
2023-06-30 15:33:06 +0330

    refactor: make oop

--- src/Monitor.php
+++ src/Monitor.php
@@ -4,87 +4,51 @@ namespace Binism\ErrorMonitoring;
 
 class Monitor
 {
+    private $basePath;
+    private $token;
+    private $host = 'http://192.168.1.66:8081';
+
+    public function __construct($token)
+    {
+        $this->basePath = dirname(debug_backtrace()[0]['file']);
+        $this->token = $token;
+    }
+
     public static function init($token)
     {
-        try {
-            //calcute base path of project
-            $basePath = dirname(debug_backtrace()[0]['file']);
+        $errorHandler = new static($token);
+        $errorHandler->handleErrors();
+    }
 
-            //error listener
-            set_error_handler(function ($errno, $errstr, $errfile, $errline) use ($basePath, $token) {
+    public function handleErrors()
+    {
+        set_error_handler([$this, 'errorHandler']);
+    }
 
+    public function errorHandler($errno, $errstr, $errfile, $errline)
+    {
+        $systems = ['systems_domain' => $_SERVER['SERVER_NAME'], 'systems_dbName' => 'dbname'];
 
-                //just work in fullsana
-                if (isset($_SERVER['SERVER_NAME']) and $_SERVER['SERVER_NAME'] === 'domain.test') {
-                    // $serverValues = $_SERVER;
-
-                    // $serverValues = array_combine(
-                    //     array_map(function($k){ return 'Server_'.$k; }, array_keys($serverValues)),
-                    //     $serverValues
-                    // );
-
-
-                    //system informations array
-                    $systems = [
-                        'systems_domain' => $_SERVER['SERVER_NAME'],
-                        // 'systems_dbName' => DB::connection()->getPdo()->query('select database()')->fetchColumn(),
-                        'systems_dbName' => 'dbname',
-                    ];
-
-
-                    // calcute trace according to base path
-                    $trace = debug_backtrace();
-                    foreach ($trace as $key => $t) {
-                        $trace[$key]['file'] = str_replace($basePath, "basePath", $t['file']);
-                        foreach ($trace[$key]['args'] as $argkey => $arg) {
-                            if (gettype($arg) === 'string')
-                                $trace[$key]['args'][$argkey] = str_replace($basePath, "basePath", $arg);
-                        }
-                    }
-
-
-                    //error information array
-                    $error_array = [
-                        'errorlanguage' => 'php',
-                        'errorMessage' => $errstr,
-                        'errorCode' => $errno,
-                        'errorFile' => str_replace($basePath, "basePath", $errfile),
-                        'errorLine' => $errline,
-                        'errorTrace' => json_encode($trace)
-                    ];
-
-                    //request body param
-                    $postFields = array_merge($error_array/*,$serverValues*/, $systems);
-
-                    //send information as http request 
-
-
-                    // create curl resource
-                    $ch = curl_init();
-
-                    // set url
-                    curl_setopt($ch, CURLOPT_URL, "http://error-monitoring.pzhame.ir/api/$token/errors");
-
-                    //return the transfer as a string
-                    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-
-                    curl_setopt($ch, CURLOPT_POST, true);
-
-                    curl_setopt(
-                        $ch,
-                        CURLOPT_POSTFIELDS,
-                        $postFields
-                    );
-                    // $output contains the output string
-                    $output = curl_exec($ch);
-
-                    // close curl resource to free up system resources
-                    curl_close($ch);
-                    echo $output;
+        $trace = debug_backtrace();
+        foreach ($trace as $key => $t) {
+            $trace[$key]['file'] = str_replace($this->basePath, "basePath", $t['file']);
+            foreach ($trace[$key]['args'] as $argkey => $arg) {
+                if (gettype($arg) === 'string') {
+                    $trace[$key]['args'][$argkey] = str_replace($this->basePath, "basePath", $arg);
                 }
-            });
-        } finally {
+            }
         }
+
+        $error_array = ['errorlanguage' => 'php', 'errorMessage' => $errstr, 'errorCode' => $errno, 'errorFile' => str_replace($this->basePath, "basePath", $errfile), 'errorLine' => $errline, 'errorTrace' => json_encode($trace),];
+
+        $postFields = array_merge($error_array, $systems);
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "{$this->host}/api/{$this->token}/errors");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postFields);
+        $output = curl_exec($ch);
+        curl_close($ch);
     }
 }
